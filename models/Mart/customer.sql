9274c6db17ac953f7994f0b6bf1f079024b528ec
seongyel2014@gmail.com <seongyel2014@gmail.com>
2021-06-01 23:05:38 +0000

    mart customer

---
+++ models/Mart/customer.sql
@@ -0,0 +1,39 @@
+with payment as (
+
+    select * from {{ ref('stg_payment_sample')}}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_order_Apr_sample') }}
+
+),
+
+customer_orders as (
+
+    select
+        userid,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(id) as number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customer_orders
+
+)
+
+select * from final
