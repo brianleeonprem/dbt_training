ba0ccb3442cf014906eeb3025869a1fc9754d4fa
seongyel2014@gmail.com <seongyel2014@gmail.com>
2021-06-02 17:36:55 +0000

    macro training

--- models/STG/stg_payment_sample.sql
+++ models/STG/stg_payment_sample.sql
@@ -1,7 +1,8 @@
 
 with PAYMENTS as (
 select
-*, case when amount>2000 then amount else 0 end large_amt
+*, case when amount>2000 then amount else 0 end large_amt,
+{{testmacro('amount')}} as twice_amt
 from DBT_TRAINING.PUBLIC.PAYMENTS
 )
 
